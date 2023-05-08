aa0ac410ef553e98abae53094abb4e00cade1801
rikarani <46773591+rikarani@users.noreply.github.com>
2023-05-09 00:50:03 +0700

    first commit

---
+++ src/handler.js
@@ -0,0 +1,53 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const nanoid = require("nanoid");
+const notes = require("./notes");
+
+const addNoteHandler = (request, h) => {
+  const { title, tags, body } = request.payload;
+
+  const id = nanoid.nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    id,
+    title,
+    createdAt,
+    updatedAt,
+    tags,
+    body,
+  };
+
+  // Tambahkan Notes kedalam array
+  notes.push(newNote);
+
+  // cek apakah note berhasil ditambahkan atau tidak
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: "success",
+      message: "Catatan berhasil ditambahkan",
+      data: {
+        noteId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+  const response = h.response({
+    status: "fail",
+    message: "Catatan gagal ditambahkan",
+  });
+  response.code(500);
+  return response;
+};
+
+const getAllNotesHandler = () => ({
+  status: "success",
+  data: {
+    notes,
+  },
+});
+
+module.exports = { addNoteHandler, getAllNotesHandler };
